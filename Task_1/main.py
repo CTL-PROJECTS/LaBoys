90333d679a0b73a16e39c410cc5b2a9b3e118888
OS-carkueng <oscarkueng@icloud.com>
2026-09-24 21:18:02 +0200

    task 1 plan

---
+++ Task_1/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# init grind with random conc
+
+#init paramenters
+
+#main time step loop:
+    #call function to calculate new state
+
+#display plot at t=0 and t=end
